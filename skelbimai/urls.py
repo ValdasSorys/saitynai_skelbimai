57790b246d1bc7d1bfe6b73018ae4ed412b44f9c
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2020-10-07 18:54:19 +0300

    Mock api finished
    Mock api for:
    categories,
    ads,
    comments,
    users

--- skelbimai/urls.py
+++ skelbimai/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from . import views
+from . import api
 
 urlpatterns = [
-    path('categories/', views.categoryAPI1, name='categoryAPI1'),
-    path('categories/<int:index>', views.categoryAPI2, name='categoryAPI2'),
+    path('categories/', api.categoryAPI1, name='categoryAPI1'),
+    path('categories/<int:index>', api.categoryAPI2, name='categoryAPI2'),
+    path('categories/<int:index>/ads', api.adAPI1, name = "adAPI1"),
+    path('ads/', api.adAPI2, name = "adAPI2"),
+    path('ads/<int:index>', api.adAPI3, name = "adAPI3"),
+    path('ads/<int:index>/comments/', api.commentAPI1, name = "commentAPI1"),
+    path('ads/<int:index1>/comments/<int:index2>', api.commentAPI2, name = "commentAPI2"),
+    path('users/', api.userAPI1, name = "userAPI1"),
+    path('users/<int:index>/', api.userAPI2, name = "userAPI2"),
 ]
